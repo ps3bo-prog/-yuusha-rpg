ae26ef1770484b5a15e30e96e4738187c1a5f622
ps3bo-prog <ps3bo@icloud.com>
2026-08-24 00:59:29 +0900

    Delete ChunkManager.js

--- ChunkManager.js
+++
@@ -1,6 +0,0 @@
-// REAL v19 ChunkManager scaffold
-// Future home for streamed open-world chunks and region loading.
-export class ChunkManager {
-  constructor({ chunkSize = 24 } = {}) { this.chunkSize = chunkSize; }
-  update() {}
-}
