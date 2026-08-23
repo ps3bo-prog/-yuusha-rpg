5b6db042f65df0f760163275a27588a569a5b8d4
ps3bo-prog <ps3bo@icloud.com>
2026-08-24 01:00:00 +0900

    Delete PlayerController.js

--- PlayerController.js
+++
@@ -1,6 +0,0 @@
-// REAL v19 PlayerController scaffold
-// Future home for movement, camera-relative input, animation state and equipment visuals.
-export class PlayerController {
-  constructor({ player, camera }) { this.player = player; this.camera = camera; }
-  update() {}
-}
